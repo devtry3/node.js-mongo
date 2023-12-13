aad89f2fafa4537f870dbf82d33926213cdc35fb
Stanisław Cieślicki <stahoo@poczta.onet.pl>
2023-12-13 22:17:11 +0100

    004-insertMany

--- src/index.js
+++ src/index.js
@@ -22,11 +22,26 @@ async function run() {
 
         const dbo = client.db(process.env.DATABASE_DB);
 
-        const myObj = { name: "Company Inc", address: "Highway 37" };
+        const myObj = [
+            { name: 'John', address: 'Highway 71'},
+            { name: 'Peter', address: 'Lowstreet 4'},
+            { name: 'Amy', address: 'Apple st 652'},
+            { name: 'Hannah', address: 'Mountain 21'},
+            { name: 'Michael', address: 'Valley 345'},
+            { name: 'Sandy', address: 'Ocean blvd 2'},
+            { name: 'Betty', address: 'Green Grass 1'},
+            { name: 'Richard', address: 'Sky st 331'},
+            { name: 'Susan', address: 'One way 98'},
+            { name: 'Vicky', address: 'Yellow Garden 2'},
+            { name: 'Ben', address: 'Park Lane 38'},
+            { name: 'William', address: 'Central st 954'},
+            { name: 'Chuck', address: 'Main Road 989'},
+            { name: 'Viola', address: 'Sideway 1633'}
+        ];
 
-        await dbo.collection(process.env.COLLECTION).insertOne(myObj);
+        const result = await dbo.collection(process.env.COLLECTION).insertMany(myObj);
 
-        console.log("1 document inserted!");
+        console.log(`Number of documents inserted: ${result.insertedCount}`);
     } finally {
         await client.close();
     }
